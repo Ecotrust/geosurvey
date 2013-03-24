8c8e56cdd477dd21b0e13e3f666f4a29ffc56b0b
Edwin Knuth <eknuth@gmail.com>
2013-03-23 20:11:01 -0700

    test and controller for survey list

--- app/scripts/controllers/SurveyList.js
+++ app/scripts/controllers/SurveyList.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('askApp')
-  .controller('SurveyListCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SurveyListCtrl', function ($scope, $http) {
+      	
+  	$http.get('surveys/all.json').success(function(data) {
+  	  $scope.surveys = data;
+  	})
+
   });
--- test/spec/controllers/SurveyList.js
+++ test/spec/controllers/SurveyList.js
@@ -5,18 +5,25 @@ describe('Controller: SurveyListCtrl', function () {
   // load the controller's module
   beforeEach(module('askApp'));
 
-  var SurveyListCtrl,
-    scope;
+  var SurveyListCtrl, $httpBackend, scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller) {
-    scope = {};
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('surveys/all.json').respond(
+      [{name:'Cheese Survey'}, {name:'Trees Survey'}]
+    );
+
+    scope = $rootScope.$new();
+
     SurveyListCtrl = $controller('SurveyListCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a list of surveys to the scope', function () {
+    expect(scope.surveys).toBeUndefined();
+    $httpBackend.flush();
+    expect(scope.surveys.length).toBe(2);
   });
 });
