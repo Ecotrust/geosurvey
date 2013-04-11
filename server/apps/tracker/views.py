caaf95164a36ee4e4ec0dd790c1e657bdcdc6929
Edwin Knuth <eknuth@ecotrust.org>
2013-04-11 13:11:44 -0700

    stubbing out view with template

--- server/apps/tracker/views.py
+++ server/apps/tracker/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def register(request, template='register.html'):
+def register(request, template='tracker/register.html'):
     return render_to_response(template, RequestContext(request, {}))
