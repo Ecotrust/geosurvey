ad53d641ed58a5178cbf961b33bdfffb5f776ed4
Edwin Knuth <eknuth@ecotrust.org>
2013-04-11 15:52:16 -0700

    updating e2e to use django, adding marco scenario

---
+++ test/e2e/marco/marcoScenarios.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+describe('Geosurvey App', function() {
+
+    beforeEach(function() {
+        browser().navigateTo('/survey/index.html#/survey/marco/a352a35e-89a2-43b9-894c-35927145f19e');
+    });
+
+    it('should redirect index.html to index.html', function() {
+        expect(browser().location().url()).toBe("/survey/marco/a352a35e-89a2-43b9-894c-35927145f19e");
+
+        expect(element('h1').text()).toBe('MARCO Survey');
+
+    });
+
+
+
+});
