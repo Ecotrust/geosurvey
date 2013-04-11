3b6eb251342e274f967a9f7f7baa7e53e0afb476
Edwin Knuth <eknuth@ecotrust.org>
2013-04-11 14:42:10 -0700

    admin update

--- apps/tracker/admin.py
+++ apps/tracker/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from tracker.models import Respondant
 
 
-admin.site.register(Respondant)
+class RespondantAdmin(admin.ModelAdmin):
+    readonly_fields=('uuid',)
+
+admin.site.register(Respondant, RespondantAdmin)
