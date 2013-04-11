0640da2ac6df239e2d1ff2caa419db5151e487bf
Edwin Knuth <eknuth@ecotrust.org>
2013-04-11 16:21:35 -0700

    removing respondant from

--- apps/survey/models.py
+++ apps/survey/models.py
@@ -1,18 +1,2 @@
 from django.db import models
 
-import datetime
-import uuid
-
-
-def make_uuid():
-    return str(uuid.uuid4())
-
-
-class Respondant(models.Model):
-    ts = models.DateTimeField(default=datetime.datetime.now())
-    email = models.EmailField(max_length=254)
-
-    uuid = models.CharField(max_length=36, primary_key=True, default=make_uuid, editable=False)
-
-    def __str__(self):
-        return "%s" % self.email
