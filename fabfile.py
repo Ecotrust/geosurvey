2d7187d674834b104a56ddde8c1d8d35c27481d8
Edwin Knuth <eknuth@ecotrust.org>
2013-04-11 12:34:16 -0700

    adding collect static

--- fabfile.py
+++ fabfile.py
@@ -20,14 +20,8 @@ def all():
    env.hosts = [dev_server, prod_server]
 
 
-def install_requirements():
-	run('cd /vagrant/server && /usr/local/venv/geosurvey/bin/pip install -r REQUIREMENTS')
-
-def install_django():
-	run('/vargrant/server && /usr/local/venv/geosurvey/bin/python manage.py syncdb && /usr/local/venv/geosurvey/bin/python manage.py migrate && /usr/local/venv/geosurvey/bin/python manage.py install_media')	
-
-def install_media():
-	run('cd /vagrant/server && /usr/local/venv/geosurvey/bin/python manage.py install_media')	
+def collectstatic():
+	run('cd /vagrant/server && /usr/local/venv/geosurvey/bin/python manage.py collectstatic')	
 
 
 def run_server():
