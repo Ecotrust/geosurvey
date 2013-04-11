568b7cf5fec0b808c2cfbe6bff035f23d37a6c6a
Edwin Knuth <eknuth@ecotrust.org>
2013-04-11 15:06:06 -0700

    email + stubbed out templates

--- server/apps/tracker/views.py
+++ server/apps/tracker/views.py
@@ -2,8 +2,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+
 
 from tracker.models import Respondant
+
+
+def send_email(email, uuid):
+
+    plaintext = get_template('tracker/email.txt')
+    htmly = get_template('tracker/email.html')
+
+    d = Context({'uuid': uuid})
+
+    subject, from_email, to = 'Take The Survey', 'eknuth@ecotrust.org', email
+    text_content = plaintext.render(d)
+    html_content = htmly.render(d)
+
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
 
 
 def register(request, template='tracker/register.html'):
@@ -12,7 +32,7 @@ def register(request, template='tracker/register.html'):
         email = request.POST.get('emailAddress', None)
         if email is not None:
             respondant, created = Respondant.objects.get_or_create(email=email)
-            print respondant.uuid
+            send_email(respondant.email, respondant.uuid)
             return render_to_response('tracker/thankyou.html', RequestContext(request, {}))
 
     return render_to_response(template, RequestContext(request, {}))
