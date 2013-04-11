505cdd0a826725e4b224e96c38e5bd4305023367
Edwin Knuth <eknuth@ecotrust.org>
2013-04-11 14:07:15 -0700

    thank you

--- server/apps/tracker/views.py
+++ server/apps/tracker/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+
 def register(request, template='tracker/register.html'):
+    if request.POST:
+        return render_to_response('tracker/thankyou.html', RequestContext(request, {}))
+
     return render_to_response(template, RequestContext(request, {}))
