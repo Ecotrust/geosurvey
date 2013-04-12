e19f844f2039c67a2c21b382b95015694162bb75
Edwin Knuth <eknuth@ecotrust.org>
2013-04-12 16:23:42 -0700

    working response

--- apps/survey/views.py
+++ apps/survey/views.py
@@ -2,8 +2,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.shortcuts import get_object_or_404
 
 import simplejson
+
+from apps.survey.models import Survey, Question, Response
+from apps.tracker.models import Respondant
 
 
 def survey(request, template='survey/survey.html'):
@@ -11,4 +15,14 @@ def survey(request, template='survey/survey.html'):
 
 
 def answer(request, survey_slug, question_slug, uuid): #, survey_slug, question_slug, uuid):
-    return HttpResponse(simplejson.dumps({'success': "%s/%s/%s" % (survey_slug, question_slug, uuid)}))
+    if request.POST:
+        survey = get_object_or_404(Survey, slug=survey_slug)
+        question = get_object_or_404(Question, slug=question_slug)
+        respondant = get_object_or_404(Respondant, uuid=uuid)
+        print request.POST.keys()
+        response, created = Response.objects.get_or_create(question=question,respondant=respondant)
+        response.answer = "test"
+        response.save()
+
+        return HttpResponse(simplejson.dumps({'success': "%s/%s/%s" % (survey_slug, question_slug, uuid)}))
+    return HttpResponse(simplejson.dumps({'success': False}))
