57ec01fbf4957dfba8218f87e103c29da352e915
Edwin Knuth <eknuth@ecotrust.org>
2013-04-12 12:21:44 -0700

    allowing slug for survey api urls

--- apps/survey/api.py
+++ apps/survey/api.py
@@ -1,5 +1,7 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from tastypie import fields, utils
+
+from django.conf.urls.defaults import url
 
 from survey.models import Survey, Question, Option
 
@@ -20,3 +22,8 @@ class SurveyResource(ModelResource):
 
     class Meta:
         queryset = Survey.objects.all()
+
+    def override_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<slug>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
