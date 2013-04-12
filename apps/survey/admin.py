2226ce51a162c285c26f97059347d6d49f048563
Edwin Knuth <eknuth@ecotrust.org>
2013-04-12 11:06:36 -0700

    adding tastypie, survey model/admin, using backend for survey list

--- apps/survey/admin.py
+++ apps/survey/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from survey.models import Survey
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',),}
+
+admin.site.register(Survey, SurveyAdmin)
