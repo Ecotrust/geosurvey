b787ba87ca933a0c23b7775ec27030ef7bc234f5
Edwin Knuth <eknuth@ecotrust.org>
2013-04-12 14:49:45 -0700

    juggling urls, added response url and empty view

--- server/apps/survey/views.py
+++ server/apps/survey/views.py
@@ -10,5 +10,5 @@ def survey(request, template='survey/survey.html'):
     return render_to_response(template, RequestContext(request, {}))
 
 
-def answer(request):
-    return HttpResponse(simplejson.dumps({'success': True}))
+def answer(request, survey_slug, question_slug, uuid): #, survey_slug, question_slug, uuid):
+    return HttpResponse(simplejson.dumps({'success': "%s/%s/%s" % (survey_slug, question_slug, uuid)}))
