d5be16a0093505ed6348bb32e5f3302a5115bcd5
Edwin Knuth <eknuth@ecotrust.org>
2013-04-15 14:07:04 -0700

    passing test for respondant detail

--- test/spec/controllers/RespondantDetail.js
+++ test/spec/controllers/RespondantDetail.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var token = 'csrftoken';
+
 describe('Controller: RespondantDetailCtrl', function() {
 
     // load the controller's module
     beforeEach(module('askApp'));
 
     var RespondantDetailCtrl, $httpBackend, scope;
+    
 
     // Initialize the controller and a mock scope
     beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
@@ -15,22 +18,24 @@ describe('Controller: RespondantDetailCtrl', function() {
         $routeParams.surveySlug = 'test-survey';
         $routeParams.uuidSlug = 'uuid-xxxxy';
 
+        $httpBackend.expectGET('/api/v1/respondant/?format=json').respond({});
+
         RespondantDetailCtrl = $controller('RespondantDetailCtrl', {
             $scope: scope
         });
 
+        $httpBackend.flush();
         
     }));
 
 
     afterEach(function() {
-        // $httpBackend.verifyNoOutstandingExpectation();
-        // $httpBackend.verifyNoOutstandingRequest();
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
     });
 
     it('should attach a response to the scope', function() {
-        $httpBackend.expectGET('/api/v1/respondant/?format=json').respond({});
-
-        //expect(scope.awesomeThings.length).toBe(3);
+        
+        expect(scope.awesomeThings.length).toBe(3);
     });
 });
