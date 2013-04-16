97c8fb85aa5535da212f1dd6f5e20d75e4564233
Edwin Knuth <eknuth@ecotrust.org>
2013-04-16 11:28:16 -0700

    merging in changes, seperating forms for question type

--- app/scripts/controllers/SurveyDetail.js
+++ app/scripts/controllers/SurveyDetail.js
@@ -22,14 +22,7 @@ angular.module('askApp')
         return nextQuestion ? nextQuestion.slug: null;
     };
 
-
-
-    $scope.answerQuestion = function(answer) {
-        var valid = $scope.validate();
-        if (!valid) {
-            return;
-        }
-
+    $scope.answerQuestion = function (answer) {
         var url = ['/respond/answer',$scope.survey.slug, $routeParams.questionSlug, $routeParams.uuidSlug].join('/'),
             nextQuestion = $scope.getNextQuestion(),
             nextUrl = ['survey', $scope.survey.slug, nextQuestion, $routeParams.uuidSlug].join('/');
@@ -45,31 +38,6 @@ angular.module('askApp')
             }).success(function(data) {
                 $location.path(nextUrl);
             });
-        }
-        
-
-    };
-
-    $scope.validate = function () {
-        // Only perform validation for question types that need it.
-        if (questionForm && questionForm.integerAnswer) {
-            return (questionForm.integerAnswer.validity && questionForm.integerAnswer.validity.valid);
-        }
-        else {
-            return true;
-        }   
-    };
-
-    $scope.onlyDigits = function($event) {
-        console.log("onlyDigits()");
-        $event = $event || window.event;
-        var charCode = $event.which || $event.keyCode;
-        var charStr = String.fromCharCode(charCode);
-
-        if (charCode > 57 ||  charCode < 48) {
-          // prevent non-numeric values from showing up
-          $event.preventDefault();
-          return false;
         }
     };
 
