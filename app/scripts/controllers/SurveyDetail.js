027fd949668f119342060842eeec7607c2ae232d
Edwin Knuth <eknuth@ecotrust.org>
2013-04-17 14:20:57 -0700

    adding bootstrap ui, starting modal

--- app/scripts/controllers/SurveyDetail.js
+++ app/scripts/controllers/SurveyDetail.js
@@ -1,7 +1,13 @@
 'use strict';
 
+function TestDialogController($scope, dialog){
+  $scope.close = function(result){
+    dialog.close(result);
+  };
+}
+
 angular.module('askApp')
-    .controller('SurveyDetailCtrl', function($scope, $routeParams, $http, $location, offlineSurvey) {
+    .controller('SurveyDetailCtrl', function($scope, $routeParams, $http, $location, $dialog, offlineSurvey) {
 
     $http.get('/api/v1/survey/' + $routeParams.surveySlug + '/?format=json').success(function(data) {
         $scope.survey = data;
@@ -14,14 +20,17 @@ angular.module('askApp')
 
         if ($scope.question.type === 'map-multipoint') {
             $scope.map = {
-                center: { lat: 42.505, lng: -122.59 },
+                center: {
+                    lat: 42.505,
+                    lng: -122.59
+                },
                 zoom: 6,
                 marker: {
-                       visibility: true,
-                       lat: 42.505,
-                       lng: -122.59,
+                    visibility: true,
+                    lat: 42.505,
+                    lng: -122.59,
 
-                    },
+                },
                 msg: null
             }
             $scope.locations = [];
@@ -30,30 +39,58 @@ angular.module('askApp')
 
     });
 
-    $scope.confirmLocation = function () {
+    $scope.confirmLocation = function() {
         $scope.locations.push($scope.activeMarker);
         $scope.activeMarker = false;
+        var t = '<div class="modal-header">' +
+            '<h1>This is the title</h1>' +
+            '</div>' +
+            '<div class="modal-body">' +
+            '<p>Enter a value to pass to <code>close</code> as the result: <input ng-model="result" /></p>' +
+            '</div>' +
+            '<div class="modal-footer">' +
+            '<button ng-click="close(result)" class="btn btn-primary" >Close</button>' +
+            '</div>';
+
+
+
+        var d = $dialog.dialog({
+            backdrop: true,
+            keyboard: true,
+            backdropClick: false,
+            template: t, // OR: templateUrl: 'path/to/view.html',
+            controller: 'TestDialogController'
+        });
+        d.open().then(function(result) {
+            if (result) {
+                alert('dialog closed with result: ' + result);
+            }
+        });
     }
 
-    $scope.cancelConfirmation = function () {
+    $scope.cancelConfirmation = function() {
         $scope.activeMarker = false;
     }
 
-    $scope.addMarker = function () {
+    $scope.addMarker = function() {
         $scope.map.marker.visibility = true;
-        $scope.activeMarker = {lat: $scope.map.marker.lat, lng: $scope.map.marker.lng};
+        $scope.activeMarker = {
+            lat: $scope.map.marker.lat,
+            lng: $scope.map.marker.lng
+        };
     }
 
     $scope.getNextQuestion = function() {
         // should return the slug of the next question
         var nextQuestion = $scope.survey.questions[_.indexOf($scope.survey.questions, $scope.question) + 1];
 
-        return nextQuestion ? nextQuestion.slug: null;
+        return nextQuestion ? nextQuestion.slug : null;
     };
 
     $scope.answerQuestion = function(answer) {
-        var url = ['/respond/answer',$scope.survey.slug, $routeParams.questionSlug, $routeParams.uuidSlug].join('/'),
-            nextQuestion = $scope.getNextQuestion(), nextUrl;
+        var url = ['/respond/answer', $scope.survey.slug, $routeParams.questionSlug, $routeParams.uuidSlug].join('/'),
+            nextQuestion = $scope.getNextQuestion(),
+            nextUrl;
 
         if (nextQuestion) {
             nextUrl = ['survey', $scope.survey.slug, nextQuestion, $routeParams.uuidSlug].join('/');
@@ -67,8 +104,12 @@ angular.module('askApp')
             $http({
                 url: url,
                 method: 'POST',
-                data: {"answer": answer},
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                data: {
+                    "answer": answer
+                },
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
             }).success(function(data) {
                 $location.path(nextUrl);
             });
