50a37b2560a06b1069be30f030032152032fc8f0
Tim Glaser <tim.glaser@ecotrust.org>
2013-04-18 14:02:31 -0700

    using the options_json field for json options

--- app/scripts/controllers/SurveyList.js
+++ app/scripts/controllers/SurveyList.js
@@ -4,7 +4,5 @@ angular.module('askApp')
   .controller('SurveyListCtrl', function ($scope, $http) {
   	$http.get('/api/v1/survey/?format=json').success(function(data) {
   	  $scope.surveys = data.objects;
-  	}).error(function(data, status, headers, config) {
-  		alert('fail');
   	});
   });
