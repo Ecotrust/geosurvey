51ddef9bb59d2a87840918064e93489517468761
Edwin Knuth <eknuth@ecotrust.org>
2013-05-01 13:55:25 -0700

    removing gis form installed apps

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'django.contrib.gis',
+    # 'django.contrib.gis',
 
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
