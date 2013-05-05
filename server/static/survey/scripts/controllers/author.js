a489bcb18371264ef0d2c0310009964e21333715
Edwin Knuth <eknuth@ecotrust.org>
2013-05-05 10:28:56 -0700

    unit tests using fixture

--- server/static/survey/scripts/controllers/author.js
+++ server/static/survey/scripts/controllers/author.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('askApp')
-  .controller('AuthorCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AuthorCtrl', function ($scope, $http, $routeParams) {
+        $http.get('/api/v1/survey/' + $routeParams.surveySlug + '/?format=json').success(function(data) {
+            $scope.survey = data;
+        });
   });
