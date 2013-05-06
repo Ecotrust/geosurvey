6b8b84d02ccf942b370fdde9af766ecdaaeef5fe
Edwin Knuth <eknuth@ecotrust.org>
2013-05-06 10:06:38 -0700

    selecting question type

--- static/survey/components/angular-ui/build/angular-ui.js
+++ static/survey/components/angular-ui/build/angular-ui.js
@@ -1091,7 +1091,6 @@ angular.module('ui.directives').directive('uiSortable', [
         var onReceive, onRemove, onStart, onUpdate, opts, _receive, _remove, _start, _update;
 
         opts = angular.extend({}, uiConfig.sortable, scope.$eval(attrs.uiSortable));
-
         if (ngModel) {
 
           ngModel.$render = function() {
@@ -1185,7 +1184,9 @@ angular.module('ui.directives').directive('uiSortable', [
         }
 
         // Create sortable
+
         element.sortable(opts);
+        debugger;
       }
     };
   }
