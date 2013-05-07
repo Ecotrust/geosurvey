93daf42b9473157df2cf7e48445e81e7ef8458e6
Edwin Knuth <eknuth@ecotrust.org>
2013-05-07 16:35:36 -0700

    making anon responses configurable on the survey

--- server/apps/survey/views.py
+++ server/apps/survey/views.py
@@ -24,7 +24,7 @@ def delete_responses(request, uuid, template='survey/delete.html'):
 
 def survey(request, survey_slug=None, template='survey/survey.html'):
     if survey_slug is not None:
-        survey = get_object_or_404(Survey, slug=survey_slug)
+        survey = get_object_or_404(Survey, slug=survey_slug, anon=True)
         respondant = Respondant(survey=survey, email="anon@example.com")
         respondant.save()
         return redirect("/respond#/survey/%s/%s" % (survey.slug, respondant.uuid))
