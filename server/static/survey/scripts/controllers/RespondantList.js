881e25ef5149a35df63c9630ddca61c9a80adb83
Edwin Knuth <eknuth@gmail.com>
2013-05-12 20:31:01 -0700

    completes/incompletes

--- server/static/survey/scripts/controllers/RespondantList.js
+++ server/static/survey/scripts/controllers/RespondantList.js
@@ -2,7 +2,7 @@
 
 angular.module('askApp')
   .controller('RespondantListCtrl', function ($scope, $http, $routeParams) {
-    $http.get('/api/v1/survey/' + $routeParams.surveySlug + '/?format=json').success(function(data) {
+    $http.get('/api/v1/surveyreport/' + $routeParams.surveySlug + '/?format=json').success(function(data) {
       $scope.survey = data;
     }).success(function () {
     	$http.get('api/v1/respondant/?format=json&limit=5&survey__slug__exact=' + $routeParams.surveySlug).success(function(data) {
