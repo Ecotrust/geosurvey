b663d7036bec65257c5b9e7bff24886a022aed2d
Edwin Knuth <eknuth@ecotrust.org>
2013-06-03 12:38:16 -0700

    multi filtering

--- server/static/survey/scripts/controllers/RespondantList.js
+++ server/static/survey/scripts/controllers/RespondantList.js
@@ -4,7 +4,24 @@ angular.module('askApp')
     .controller('RespondantListCtrl', function($scope, $http, $routeParams) {
     $http.get('/api/v1/surveyreport/' + $routeParams.surveySlug + '/?format=json').success(function(data) {
         $scope.survey = data;
+        
+        $scope.$watch('filters', function (newFilters) {
+            console.log(newFilters);
+        
+        }, true);
 
+        _.each($scope.survey.questions, function (question) {
+            // save a reference to filter questions which are specified by uri
+            question.filters = {};
+            if (question.visualize && question.filter_questions) {
+                question.filterQuestions = [];
+                _.each(question.filter_questions, function (filterQuestion) {
+                    question.filterQuestions.push($scope.getQuestionByUri(filterQuestion));
+                });
+
+            }
+
+        });
 
     }).success(function() {
         $http.get('api/v1/respondant/?format=json&limit=5&survey__slug__exact=' + $routeParams.surveySlug).success(function(data) {
@@ -13,6 +30,9 @@ angular.module('askApp')
         });
     });
 
+    $scope.getQuestionByUri = function (uri) {
+        return _.findWhere($scope.survey.questions, {'resource_uri': uri});
+    };
 
     $scope.getQuestionBySlug = function (slug) {
 		return _.findWhere($scope.survey.questions, {'slug': slug});
