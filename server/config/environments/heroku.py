b30fe6105b11ce150fe79ccdb1c7ca6a60da62b6
Edwin Knuth <eknuth@ecotrust.org>
2013-06-04 17:35:07 -0700

    no s3 for the moment

--- server/config/environments/heroku.py
+++ server/config/environments/heroku.py
@@ -37,17 +37,6 @@ if redis is not None:
         }
     }
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'marco-survey-ewk'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-
 COMPRESS_ENABLED = False
   
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
