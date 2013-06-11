1a73c964332092c4d7971b713a883f116468231e
Edwin Knuth <eknuth@ecotrust.org>
2013-06-11 11:34:55 -0700

    removing comma from urls

--- server/config/urls.py
+++ server/config/urls.py
@@ -54,4 +54,4 @@ urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.HEROKU:
-    urlpatterns += (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    urlpatterns += (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
