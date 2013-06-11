ceb45415ca5789603135a9e59bc963dc30146f9f
Edwin Knuth <eknuth@ecotrust.org>
2013-06-11 11:54:40 -0700

    update statics

--- server/config/urls.py
+++ server/config/urls.py
@@ -50,8 +50,9 @@ urlpatterns = patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.HEROKU:
 #     urlpatterns += patterns('',(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
