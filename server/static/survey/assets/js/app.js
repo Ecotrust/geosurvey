6a6d57a6f29511894c803345d446d0852bc04a96
Edwin Knuth <eknuth@ecotrust.org>
2013-07-01 11:58:05 -0700

    relative loading for mobile

--- server/static/survey/assets/js/app.js
+++ server/static/survey/assets/js/app.js
@@ -12,43 +12,43 @@ angular.module('askApp', ['ui', 'leaflet.directive', 'ui.bootstrap', 'ngGrid'])
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
     $routeProvider.when('/', {
-        templateUrl: '/static/survey/views/main.html',
+        templateUrl: 'views/main.html',
         controller: 'MainCtrl'
     })
         .when('/survey/:surveySlug/:questionSlug/offline', {
-        templateUrl: '/static/survey/views/SurveyDetail.html',
+        templateUrl: 'views/SurveyDetail.html',
         controller: 'SurveyDetailCtrl'
     })
         .when('/author/:surveySlug', {
-        templateUrl: '/static/survey/views/author.html',
+        templateUrl: 'views/author.html',
         controller: 'AuthorCtrl'
     })
         .when('/surveys', {
-        templateUrl: '/static/survey/views/SurveyList.html',
+        templateUrl: 'views/SurveyList.html',
         controller: 'SurveyListCtrl'
     })
         .when('/survey/:surveySlug/complete/:uuidSlug', {
-        templateUrl: '/static/survey/views/complete.html',
+        templateUrl: 'views/complete.html',
         controller: 'CompleteCtrl'
     })
         .when('/survey/:surveySlug/complete/:uuidSlug/:action/:questionSlug', {
-        templateUrl: '/static/survey/views/complete.html',
+        templateUrl: 'views/complete.html',
         controller: 'CompleteCtrl'
     })
         .when('/survey/:surveySlug/:questionSlug/:uuidSlug', {
-        templateUrl: '/static/survey/views/SurveyDetail.html',
+        templateUrl: 'views/SurveyDetail.html',
         controller: 'SurveyDetailCtrl'
     })
         .when('/survey/:surveySlug/:uuidSlug', {
-        templateUrl: '/static/survey/views/landing.html',
+        templateUrl: 'views/landing.html',
         controller: 'SurveyDetailCtrl'
     })
         .when('/RespondantList/:surveySlug', {
-        templateUrl: '/static/survey/views/RespondantList.html',
+        templateUrl: 'views/RespondantList.html',
         controller: 'RespondantListCtrl'
     })
         .when('/RespondantDetail/:surveySlug/:uuidSlug', {
-        templateUrl: '/static/survey/views/RespondantDetail.html',
+        templateUrl: 'views/RespondantDetail.html',
         controller: 'RespondantDetailCtrl'
     })
         .when('/survey', {
