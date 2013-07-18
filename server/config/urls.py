7fc4a7b2070ee1927efd4600dda8074a6aaf2146
Edwin Knuth <eknuth@ecotrust.org>
2013-07-17 17:05:08 -0700

    getting rid of static file server

--- server/config/urls.py
+++ server/config/urls.py
@@ -54,7 +54,7 @@ urlpatterns = patterns('',
 
     # (r'^register', survey_urls.register),
     #(r'^survey/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.SURVEY_ROOT}),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    # (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
 
 if settings.DEBUG:
