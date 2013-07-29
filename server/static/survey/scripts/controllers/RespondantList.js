b902fe8c33d5a5aad5b448cf4a0517328434ee37
Tim Glaser <tim.glaser@ecotrust.org>
2013-07-29 11:01:44 -0700

    loading indicator for dashboard

--- server/static/survey/scripts/controllers/RespondantList.js
+++ server/static/survey/scripts/controllers/RespondantList.js
@@ -2,10 +2,12 @@
 
 angular.module('askApp')
     .controller('RespondantListCtrl', function($scope, $http, $routeParams) {
+
+    $scope.showSpinner = true;
+
     $http.get('/api/v1/surveyreport/' + $routeParams.surveySlug + '/?format=json').success(function(data) {
         data.questions.reverse();
         $scope.survey = data;
-        
 
         _.each($scope.survey.questions, function (question) {
             // save a reference to filter questions which are specified by uri
@@ -19,7 +21,8 @@ angular.module('askApp')
             }
         });
         
-
+        $scope.showSpinner = false;
+        
     }).success(function() {
         $http.get('/api/v1/respondant/?format=json&limit=5&survey__slug__exact=' + $routeParams.surveySlug).success(function(data) {
             $scope.respondants = data.objects;
