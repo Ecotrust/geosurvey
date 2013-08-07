f6c60e9f13e8a2695b13a731bc78bf8c01379fa5
Tim Glaser <tim.glaser@ecotrust.org>
2013-08-07 14:33:24 -0700

    added extra registration fields

--- server/apps/account/views.py
+++ server/apps/account/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User, check_password
 from django.contrib.auth.forms import PasswordResetForm
+from account.models import UserProfile
 
 import simplejson
 
@@ -52,10 +53,27 @@ def createUser(request):
                 user = authenticate(
                     username=user.username, password=param.get('password1'))
                 login(request, user)
+                registration_dict = {
+                    'first_name': param.get('first_name'),
+                    'last_name': param.get('last_name'),
+                    'vessel_num': param.get('vessel_num'),
+                    'license_num': param.get('license_num'),
+                    'species_permit_num': param.get('species_permit_num'),
+                    'partner1_first_name': param.get('partner1_first_name'),
+                    'partner1_last_name': param.get('partner1_last_name'),
+                    'partner1_license_num': param.get('partner1_license_num'),
+                    'partner2_first_name': param.get('partner2_first_name'),
+                    'partner2_last_name': param.get('partner2_last_name'),
+                    'partner2_license_num': param.get('partner2_license_num')
+                    }
+                profile, created = UserProfile.objects.get_or_create(user=user)
+                profile.registration = simplejson.dumps(registration_dict)
+                profile.save()
                 user_dict = {
                     'username': user.username,
                     'name': ' '.join([user.first_name, user.last_name]),
-                    'is_staff': user.is_staff
+                    'is_staff': user.is_staff,
+                    'registration': profile.registration
                 }
                 return HttpResponse(simplejson.dumps({'success': True, 'user': user_dict}))
         else:
