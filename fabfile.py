c41ae566b54e6beb068850f6e89afad44161ce09
Edwin Knuth <eknuth@ecotrust.org>
2013-11-05 15:26:44 -0800

    updating fixtures

--- fabfile.py
+++ fabfile.py
@@ -136,7 +136,7 @@ def dumpdata():
     with cd(env.code_dir):
         with _virtualenv():
             _manage_py('dumpdata --format=json --indent=4 survey --exclude=survey.Respondant --exclude=survey.LocationAnswer --exclude=survey.Location --exclude=survey.MultiAnswer --exclude=survey.GridAnswer --exclude=survey.Response | gzip > apps/survey/fixtures/surveys.json.gz ')
-              
+            get('apps/survey/fixtures/surveys.json.gz', 'backups/surveys.json.gz')
 @task
 def loaddata():
     set_env_for_user('vagrant')
