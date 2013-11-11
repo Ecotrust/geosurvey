b4db565836c3c127ead866cd7ab83176eb26f5e1
Scott Fletcher <sfletche@gmail.com>
2013-11-11 10:39:16 -0800

    adding Home button and header to Profile Questions page. adding spinner to Profile Questions page while questions load.  also alphabetizing some admin Page fields. fixes #196 and #194.

--- server/static/survey/assets/js/controllers/profile.js
+++ server/static/survey/assets/js/controllers/profile.js
@@ -6,7 +6,7 @@ angular.module('askApp')
     $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
 
-    $scope.loaded=false;
+    $scope.loading=true;
     $scope.width = 0;
     // QUESTION - something causes this to be called for SurveyList, but I'm not sure what that something is...
     // ...manually calling updateSurveys here (see if app.surveys below)
@@ -16,6 +16,7 @@ angular.module('askApp')
         $scope.timer = setInterval(function () {
             $scope.width = $scope.width + 10;
         }, 500);
+
         $http.get(app.server + '/api/v1/survey/?format=json').success(function(data) {
             $scope.surveys = data.objects;
             _.each($scope.surveys, function (survey) {
@@ -24,9 +25,7 @@ angular.module('askApp')
             app.surveys = $scope.surveys;
             $scope.saveState();
             $scope.hideSurveys = false;
-            $scope.loaded = true;
             clearInterval($scope.timer);
-        
             $scope.profileQuestions = getProfileQuestions();
         });
 
@@ -34,6 +33,7 @@ angular.module('askApp')
 
     var getProfileQuestions = function () {
         var profileQuestions = [];
+
         _.each($scope.surveys, function(survey, i) {
             // QUESTION - why doesn't survey.questions include the profile questions?
             // survey.questions is no longer relevant
@@ -45,6 +45,8 @@ angular.module('askApp')
                 })
             });
         });
+
+        $scope.loading = false;
         return _.uniq(profileQuestions, false, function(question) {
             return question.slug;
         });
@@ -86,10 +88,9 @@ angular.module('askApp')
             .success(function (data) {
                 app.user.registration = registration;
                 $scope.saveState();
-                $location.path('#/');
+                $location.path('#/main');
             })
             .error(function (data) {
-                $scope.working = false;
                 if (data) {
                     $scope.showError = data;    
                 } else {
