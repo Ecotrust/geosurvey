bb1da12e0de1354f4f0ac37a5f26457ac313559b
Tim Glaser <tim.glaser@ecotrust.org>
2014-01-23 14:02:41 -0800

    Open traffic to survey.

--- server/config/urls.py
+++ server/config/urls.py
@@ -29,43 +29,44 @@ urlpatterns = patterns('',
 
     # Now directing all traffic other than dash and admin 
     # traffic to a message that the survey is closed.
-    url(r'^admin', include(admin.site.urls)),
-    url(r'^grappelli/', include('grappelli.urls')),
-    (r'^api/', include(v1_api.urls)),
-    url(r'^report/', include(report_urls)),
-    url(r'^reports', include(report_urls)),
-    url(r'^dash/(?P<survey_slug>[\w\d-]+)$', 'apps.survey.views.dash'),
-    url(r'^dash$', 'apps.survey.views.dash'),
-    url(r'^dash', include(survey_urls)),
+    # url(r'^admin', include(admin.site.urls)),
+    # url(r'^grappelli/', include('grappelli.urls')),
+    # (r'^api/', include(v1_api.urls)),
+    # url(r'^report/', include(report_urls)),
+    # url(r'^reports', include(report_urls)),
+    # url(r'^dash/(?P<survey_slug>[\w\d-]+)$', 'apps.survey.views.dash'),
+    # url(r'^dash$', 'apps.survey.views.dash'),
+    # url(r'^dash', include(survey_urls)),
     # all other traffic goes to the closed message
-    (r'^.*$', survey_urls.closed),
+    # (r'^.*$', survey_urls.closed),
 
 
     # Below is the url pattern set used while the survey was open.
 
-    # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^grappelli/', include('grappelli.urls')),
-    # (r'^api/', include(v1_api.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
+    (r'^api/', include(v1_api.urls)),
 
 
-    # url(r'^report', include(report_urls)),
-    # #anon survey user for specific survey
-    # url(r'^respond/(?P<survey_slug>[\w\d-]+)$', 'apps.survey.views.survey'),
-    # url(r'^respond/(?P<survey_slug>[\w\d-]+)/(?P<question_slug>[\w\d-]+)$', 'apps.survey.views.surveyAnonWithQuestion'),
-    # #survey responder with preassigned uuid
-    # url(r'^respond$', 'apps.survey.views.survey'),
-    # #other survey urls
-    # url(r'^respond', include(survey_urls)),
+    url(r'^report', include(report_urls)),
+    url(r'^reports', include(report_urls)),
+    #anon survey user for specific survey
+    url(r'^respond/(?P<survey_slug>[\w\d-]+)$', 'apps.survey.views.survey'),
+    url(r'^respond/(?P<survey_slug>[\w\d-]+)/(?P<question_slug>[\w\d-]+)$', 'apps.survey.views.surveyAnonWithQuestion'),
+    #survey responder with preassigned uuid
+    url(r'^respond$', 'apps.survey.views.survey'),
+    #other survey urls
+    url(r'^respond', include(survey_urls)),
 
-    # # backend urls
-    # url(r'^dash/(?P<survey_slug>[\w\d-]+)$', 'apps.survey.views.dash'),
-    # #survey responder with preassigned uuid
-    # url(r'^dash$', 'apps.survey.views.dash'),
-    # #other survey urls
-    # url(r'^dash', include(survey_urls)),
-    # url(r'^log', include(survey_urls)),
+    # backend urls
+    url(r'^dash/(?P<survey_slug>[\w\d-]+)$', 'apps.survey.views.dash'),
+    #survey responder with preassigned uuid
+    url(r'^dash$', 'apps.survey.views.dash'),
+    #other survey urls
+    url(r'^dash', include(survey_urls)),
+    url(r'^log', include(survey_urls)),
 
-    # (r'^register', survey_urls.register),
+    (r'^register', survey_urls.register),
 )
 
 if settings.DEBUG:
